ed1ebde5a98ef6849f68a497393783501d2bab54
fivesixfive <contact@fivesixfive.dev>
2022-10-11 10:32:22 -0500

    temp

--- src/main.py
+++ src/main.py
@@ -5,22 +5,31 @@
 RESOLUTION = (1920, 1080)
 
 # Imports
-import pygame as g
+from sys import argv
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import \
+    QApplication, QWidget, QMainWindow, \
+    QVBoxLayout, QHBoxLayout, \
+    QLineEdit, QLabel
 
+# Build Window
 def main():
-    # Initialize PyGame
-    g.init()
-    # Create Window
-    g.display.set_mode(RESOLUTION, g.FULLSCREEN)
-    g.display.set_caption("Virus Game")
-
-def loop():
-    while True:
-        for event in g.event.get():
-            
-        g.display.update()
+    q = QApplication(argv)
+    q.setApplicationDisplayName("Virus Game")
+    # Setup Window
+    main_window = QMainWindow()
+    main_window.setWindowTitle("Virus Game")
+    main_window.showFullScreen()
+    main_window.setCursor(Qt.BlankCursor)
+    # Setup Window Layout
+    main_layout = QVBoxLayout()
+    main_window.setLayout(main_layout)
+    # Title Layout
+    # Countdown Layout
+    # Start Event Loop
+    q.exec_()
 
 # Execute
 if __name__ == "__main__":
     main()
-    loop()
